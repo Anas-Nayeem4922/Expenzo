3ac25e57c86200d3e664fa4b540caced981d8dac
Anas Nayeem <anasnayeem97@gmail.com>
2024-12-24 00:58:32 +0530

    Added the navbar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,8 +3,8 @@ import React, { ReactNode } from 'react'
 
     function layout({children} : {children : ReactNode}) {
     return (
-        <div className='bg-slate-200 relative flex h-screen w-full flex-col items-center justify-center'>
-            <Logo auth={true}/>
+        <div className='relative flex h-screen w-full flex-col items-center justify-center'>
+            <Logo/>
             <div className='mt-12'>{children}</div>
         </div>
     )
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
     return (
         <div className='cursor-pointer flex items-center ml-3'>
             <img className='h-12 w-12' src="https://www.thegreatapps.com/application/upload/Apps/2017/03/expense-manager-22.png" alt="" />
-            <p className={`text-2xl font-extrabold ml-2 ${auth && `text-black`}`}>Expenzo</p>
+            <p className={`text-2xl font-extrabold ml-2`}>Expenzo</p>
         </div>
     )
 }
